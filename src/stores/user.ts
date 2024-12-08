bc0f71380758759f146520fb58b4344de846dc33
IceyWu <3128006406@qq.com>
2024-12-08 20:27:02 +0800

    feat: 更新登出功能以重定向到主页

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -77,6 +77,8 @@ export const useUserStore = defineStore(
     const logout = () => {
       removeToken()
       setUserInfo({})
+			// location.reload()
+			window.location.href = '/'
     }
     return {
       count,
