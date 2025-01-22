eb00417ae63c75a6166f6ede5793f21d8da1668e
an.sun <an.sun@boran-tech.com>
2025-01-22 11:01:41 +0800

    fix：添加默认头像

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -2,7 +2,7 @@ import { customDestr, deepClone, getObjVal } from '@iceywu/utils'
 
 // 获取用户头像
 export function getUserAvatar(data: any) {
-	const oldAvatar = getObjVal(data, 'avatar')
+	const oldAvatar = getObjVal(data, 'avatar') || 'https://test.wktest.cn:3001/assets/default/boy.png'
 	const baseAvatar = getObjVal(data, 'avatarInfo.url')
 	return baseAvatar || oldAvatar
 }
